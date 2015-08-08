d7e427f95c17ed656882659144aca5cfa17a35c4
tatitoli <diirhun@gmail.com>
2015-08-08 14:01:13 +0200

    OfferGroup Dao és Entity hozzáadva

---
+++ application/webshop/core/src/main/java/hu/neuron/ier/core/dao/OfferGroupDao.java
@@ -0,0 +1,19 @@
+package hu.neuron.ier.core.dao;
+
+import hu.neuron.ier.core.entity.OfferGroup;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(propagation = Propagation.SUPPORTS)
+public interface OfferGroupDao  extends JpaRepository<OfferGroup, Long> {
+
+	OfferGroup findOfferGroupByName(String name) throws Exception;
+	
+	OfferGroup findOfferGroupByItemNumber(Long itemNumber) throws Exception;
+	
+	OfferGroup findOfferGroupByID(Long id) throws Exception;
+}
--- application/webshop/core/src/main/java/hu/neuron/ier/core/entity/OfferGroup.java
+++ application/webshop/core/src/main/java/hu/neuron/ier/core/entity/OfferGroup.java
@@ -15,6 +15,8 @@ public class OfferGroup extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	private Long itemNumber;
+	private Long cost;
 	private String name;
 	private String description;
 
@@ -40,6 +42,22 @@ public class OfferGroup extends BaseEntity {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Long getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(Long itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+
+	public Long getCost() {
+		return cost;
+	}
+
+	public void setCost(Long cost) {
+		this.cost = cost;
 	}
 
 }

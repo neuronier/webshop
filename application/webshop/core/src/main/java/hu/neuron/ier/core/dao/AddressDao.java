01c663b9268a0dd6440b46fd4ad5831bbd2d6fde
KoszturNorbert <kosztur@freemail.hu>
2015-08-16 09:52:40 +0200

    AddressDao.java Address findAddressByPostcodeAndCityAndStreetAndHouse(Long postcode, String city, String street, String house) throws Exception metódus hozzáadva AddressDaoTest kiegészítve logokkal és tesztekkel

--- application/webshop/core/src/main/java/hu/neuron/ier/core/dao/AddressDao.java
+++ application/webshop/core/src/main/java/hu/neuron/ier/core/dao/AddressDao.java
@@ -2,6 +2,8 @@ package hu.neuron.ier.core.dao;
 
 import hu.neuron.ier.core.entity.Address;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
@@ -11,7 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.SUPPORTS)
 public interface AddressDao extends JpaRepository<Address, Long> {
 
-	Address findAddressByCity(String city) throws Exception;
-	
-}
+	List<Address> findAddressByCity(String city) throws Exception;
 
+	Address findAddressByPostcodeAndCityAndStreetAndHouse(Long postcode,
+			String city, String street, String house) throws Exception;
+
+}
--- application/webshop/core/src/test/java/hu/neuron/ier/core/dao/test/AddressDaoTest.java
+++ application/webshop/core/src/test/java/hu/neuron/ier/core/dao/test/AddressDaoTest.java
@@ -6,6 +6,7 @@ import hu.neuron.ier.core.entity.Address;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +33,7 @@ public class AddressDaoTest {
 
 	@Test
 	public void test1Save() {
+		logger.info("test1Save start...");
 		try {
 			address = new Address();
 			address.setCity("test");
@@ -39,41 +41,110 @@ public class AddressDaoTest {
 			address.setPostcode(123l);
 			address.setStreet("test");
 			address = addressDao.save(address);
+			logger.info("Address id: " + address.getId());
+			Address address2 = new Address();
+			address2.setCity(address.getCity());
+			address2.setHouse(address.getHouse());
+			address2.setPostcode(address.getPostcode());
+			address2.setStreet(address.getStreet());
+			address2 = addressDao.save(address2);
+			logger.info("Address2 id:" + address2.getId());
+			Assert.assertNotEquals(address.getId(), address2.getId());
+			addressDao.delete(address2);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new RuntimeException(e);
 		}
+		logger.info("test1Save end");
+
 	}
 
 	@Test
-	public void test2Find() {
+	public void test2FindAddressByCity() {
+		logger.info("test2FindAddressByCity start...");
 		try {
-			Address ad = addressDao.findAddressByCity(address.getCity());
+			List<Address> adList = addressDao.findAddressByCity(address
+					.getCity());
+			// meg kell találnia, mert létezik
+			Assert.assertTrue(adList != null);
+			List<Address> nullList = addressDao
+					.findAddressByCity("nameless city");
+			logger.info("nullList is null: " + (nullList == null));
+			if (nullList != null) {
+				logger.info("nullList size: " + nullList.size());
+			}
+			Assert.assertEquals(0, nullList.size());
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new RuntimeException(e);
 		}
-
+		logger.info("test2FindAddressByCity end");
 	}
 
 	@Test
 	public void test3FindAll() {
+		logger.info("test3FindAll start...");
 		try {
 			List<Address> addresses = addressDao.findAll();
+			Assert.assertTrue(addresses.size() > 0);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new RuntimeException(e);
 		}
+		logger.info("test3FindAll end");
 	}
 
 	@Test
-	public void test4Delete() {
+	public void test4FindAddressByPostcodeAndCityAndStreetAndHouse() {
+		logger.info("test4FindAddressByPostcodeAndCityAndStreetAndHouse start...");
+		try {
+			Address result = addressDao
+					.findAddressByPostcodeAndCityAndStreetAndHouse(
+							address.getPostcode(), address.getCity(),
+							address.getStreet(), address.getHouse());
+			logger.info("result is null: " + (result == null));
+			if (result != null) {
+				logger.info("id: " + result.getId());
+				logger.info("postcode: " + result.getPostcode());
+				logger.info("city: " + result.getCity());
+				logger.info("street: " + result.getStreet());
+				logger.info("house: " + result.getHouse());
+			}
+			logger.info("result id == address id: "
+					+ address.getId().equals(result.getId()));
+			logger.info("result postcode == address postcode: "
+					+ address.getPostcode().equals(result.getPostcode()));
+			logger.info("result city == address city: "
+					+ address.getCity().equals(result.getCity()));
+			logger.info("result street == address street: "
+					+ address.getStreet().equals(result.getStreet()));
+			logger.info("result house == address house: "
+					+ address.getHouse().equals(result.getHouse()));
+
+			Assert.assertEquals(address.getId(), result.getId());
+			Assert.assertEquals(address.getPostcode(), result.getPostcode());
+			Assert.assertEquals(address.getCity(), result.getCity());
+			Assert.assertEquals(address.getStreet(), result.getStreet());
+			Assert.assertEquals(address.getHouse(), result.getHouse());
+
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
+		logger.info("test4FindAddressByPostcodeAndCityAndStreetAndHouse end");
+
+	}
+
+	@Test
+	public void test5Delete() {
+		logger.info("test5Delete start...");
 		try {
 			addressDao.delete(address);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new RuntimeException(e);
 		}
+		logger.info("test5Delete end");
 	}
 
 }

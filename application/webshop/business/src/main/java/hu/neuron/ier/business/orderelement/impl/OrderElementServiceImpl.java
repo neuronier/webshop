6457836e0f7d77c63d79468973e96f072500c6c8
Balla Mark <balla.mark@gmail.com>
2015-08-21 22:17:36 +0200

    order dao, order service tesztelve producttype service hozzáadva

--- application/webshop/business/src/main/java/hu/neuron/ier/business/orderelement/impl/OrderElementServiceImpl.java
+++ application/webshop/business/src/main/java/hu/neuron/ier/business/orderelement/impl/OrderElementServiceImpl.java
@@ -9,29 +9,40 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.interceptor.Interceptors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
+@Stateless(mappedName = "OrderElementService", name = "OrderElementService")
+@Remote(OrderElementServiceRemote.class)
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+@Interceptors(SpringBeanAutowiringInterceptor.class)
 public class OrderElementServiceImpl implements OrderElementServiceRemote, Serializable {
 
 	private static final long serialVersionUID = 9024441429565077654L;
-	
+
 	@Autowired
 	OrderElementDao orderElementDao;
-	
+
 	@EJB
 	OrderElementConverter converter;
 
 	@Override
-	public OrderElementVO createOrderElement(OrderElementVO orderElementVO){
-		OrderElementVO vo = converter.toVO(orderElementDao.save(converter.toEntity(orderElementVO)));
+	public OrderElementVO createOrderElement(OrderElementVO orderElementVO) {
+		OrderElementVO vo = converter
+				.toVO(orderElementDao.save(converter.toEntity(orderElementVO)));
 		return vo;
 	}
 
 	@Override
 	public void deleteOrderElement(Long id) throws Exception {
 		orderElementDao.delete(id);
-		
+
 	}
 
 	@Override
@@ -40,7 +51,7 @@ public class OrderElementServiceImpl implements OrderElementServiceRemote, Seria
 		orderElementVO.setQuanty(quanty);
 		createOrderElement(orderElementVO);
 		return orderElementVO;
-		
+
 	}
 
 	@Override

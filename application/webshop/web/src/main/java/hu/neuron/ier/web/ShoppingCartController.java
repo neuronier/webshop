baf7a165dc92290b2050b5b697e41c41df4031cd
Balla Mark <balla.mark@gmail.com>
2015-08-25 23:56:35 +0200

    shoppingcart javítva

--- application/webshop/web/src/main/java/hu/neuron/ier/web/ShoppingCartController.java
+++ application/webshop/web/src/main/java/hu/neuron/ier/web/ShoppingCartController.java
@@ -1,7 +1,9 @@
 package hu.neuron.ier.web;
 
+import hu.neuron.ier.business.client.ClientSelfCareServiceRemote;
 import hu.neuron.ier.business.offer.OfferServiceRemote;
 import hu.neuron.ier.business.shoppingcart.ShoppingCartRemote;
+import hu.neuron.ier.business.vo.ClientVO;
 import hu.neuron.ier.business.vo.OfferVO;
 
 import java.io.Serializable;
@@ -9,8 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SessionScoped
 @ManagedBean(name = "shoppingCartController")
@@ -18,15 +24,20 @@ public class ShoppingCartController implements Serializable {
 
 	private static final long serialVersionUID = -4897446152048179222L;
 
+	private ClientVO currentClient;
+
 	@EJB(name = "ShoppingCartService", mappedName = "ShoppingCartService")
-	ShoppingCartRemote shoppingCartService;
+	private ShoppingCartRemote shoppingCartService;
 	@EJB(mappedName = "OfferService", name = "OfferService")
-	OfferServiceRemote offerService;
+	private OfferServiceRemote offerService;
+	@EJB(name = "ClientSelfCareService", mappedName = "ClientSelfCareService")
+	private ClientSelfCareServiceRemote clientSelfCareService;
 
 	private List<OfferVO> offers = new ArrayList<OfferVO>();
 
 	public void addOfferToShoppingCart(OfferVO offerVO) {
 		offers.add(offerVO);
+
 	}
 
 	public OfferVO findOfferInShoppingCart(Long id) {
@@ -46,6 +57,31 @@ public class ShoppingCartController implements Serializable {
 
 	public void deleteAllOfferFromShoppingCart() throws Exception {
 		offers.removeAll(offers);
+	}
+
+	public int offerCounter(OfferVO offerVO) {
+		int count = 0;
+		for (OfferVO offer : offers) {
+			if (offer == offerVO) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public void info(OfferVO offerVO) {
+		FacesContext.getCurrentInstance().addMessage(
+				null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, offerVO.getName(),
+						"a kosárba helyezve."));
+	}
+
+	public void shoppingCartToClient() {
+		currentClient = new ClientVO();
+		currentClient = clientSelfCareService.getClientByName(SecurityContextHolder.getContext()
+				.getAuthentication().getName());
+		currentClient.getShoppingCart().getOffers().addAll(offers);
 	}
 
 	public List<OfferVO> getOffers() {

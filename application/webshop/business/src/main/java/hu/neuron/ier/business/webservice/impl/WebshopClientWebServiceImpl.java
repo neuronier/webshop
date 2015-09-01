b90c3cc375b0a8e15d918a8e3a1cc0e43222b32c
Balla Mark <balla.mark@gmail.com>
2015-09-01 11:21:46 +0200

    ClientWebService javítva

--- application/webshop/business/src/main/java/hu/neuron/ier/business/webservice/impl/WebshopClientWebServiceImpl.java
+++ application/webshop/business/src/main/java/hu/neuron/ier/business/webservice/impl/WebshopClientWebServiceImpl.java
@@ -2,22 +2,17 @@ package hu.neuron.ier.business.webservice.impl;
 
 import hu.neuron.ier.business.client.ClientServiceRemote;
 import hu.neuron.ier.business.vo.ClientVO;
-import hu.neuron.ier.business.webservice.ClientWebService;
+import hu.neuron.ier.business.webservice.WebshopClientWebService;
 import hu.neuron.ier.business.webservice.vo.ClientListWebServiceVO;
 import hu.neuron.ier.business.webservice.vo.ClientWebServiceVO;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Properties;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import javax.jws.WebService;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,46 +22,17 @@ import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 @Stateless(mappedName = "ClientWebService", name = "ClientWebService")
 @WebService(name = "WebshopClientWebServicePort", serviceName = "WebshopClientWebService", targetNamespace = "http://hu.neuron", endpointInterface = "hu.neuron.ier.business.webservice.ClientWebService")
 @Interceptors(SpringBeanAutowiringInterceptor.class)
-public class ClientWebServiceImpl implements ClientWebService {
+public class WebshopClientWebServiceImpl implements WebshopClientWebService {
 
 	@Autowired
 	@Qualifier("mapper")
 	Mapper mapper;
 
+	@EJB(mappedName = "ClientService", name = "ClientService")
 	ClientServiceRemote clientService;
-
-	public void initEJB() {
-		try {
-			InputStream inputStream = this.getClass().getClassLoader()
-					.getResourceAsStream("hu/neuron/ier/business/Settings.properties");
-
-			Properties properties = new Properties();
-
-			try {
-				properties.load(inputStream);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			Hashtable<String, String> env = new Hashtable<String, String>();
-			env.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-			env.put(Context.SECURITY_PRINCIPAL, properties.getProperty("SECURITY_PRINCIPAL"));
-			env.put(Context.SECURITY_CREDENTIALS, properties.getProperty("SECURITY_CREDENTIALS"));
-			env.put(Context.PROVIDER_URL, properties.getProperty("PROVIDER_URL"));
-			Context ctx;
-
-			ctx = new InitialContext(env);
-			System.out.println("ctx  = " + ctx);
-			clientService = (ClientServiceRemote) ctx
-					.lookup("ClientService#hu.neuron.ier.business.client.ClientServiceRemote");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	@Override
 	public ClientListWebServiceVO getClientListWebMethod() {
-		initEJB();
 		List<ClientVO> clientVOs = clientService.getClientList();
 		ClientListWebServiceVO rv = new ClientListWebServiceVO();
 		rv.setList(new ArrayList<ClientWebServiceVO>());
@@ -79,8 +45,6 @@ public class ClientWebServiceImpl implements ClientWebService {
 	@Override
 	public ClientWebServiceVO createClientWebMethod(String clientId, String name, String userName,
 			String password, String email, String phoneNumber) {
-		initEJB();
-
 		ClientVO clientVO = new ClientVO();
 		clientVO.setClientId(clientId);
 		clientVO.setEmail(email);
@@ -98,7 +62,6 @@ public class ClientWebServiceImpl implements ClientWebService {
 	@Override
 	public ClientWebServiceVO updateClientByUserNameWebMethod(String clientId, String name,
 			String userName, String password, String email, String phoneNumber) {
-		initEJB();
 
 		ClientVO clientVO = clientService.findClientByName(userName);
 		clientVO.setClientId(clientId);
@@ -114,7 +77,6 @@ public class ClientWebServiceImpl implements ClientWebService {
 
 	@Override
 	public ClientWebServiceVO getClientByUserNameWebMethod(String userName) {
-		initEJB();
 
 		return mapper.map(clientService.findClientByName(userName), ClientWebServiceVO.class);
 	}
